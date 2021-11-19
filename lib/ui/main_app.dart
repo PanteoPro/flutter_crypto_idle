52a218d45d9381138465f68c0a9f1dcd93f55e09
PanteoPro <panteopro@gmail.com>
2021-11-19 12:28:15 +0300

    create main game page

--- lib/ui/main_app.dart
+++ lib/ui/main_app.dart
@@ -1,11 +1,11 @@
+import 'package:crypto_idle/ui/widgets/game/main_game_page.dart';
 import 'package:flutter/material.dart';
 
 class MainApp extends StatelessWidget {
-  /// Contructor mainApp
   const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Scaffold());
+    return const MaterialApp(home: MainGamePage());
   }
 }
---
+++ lib/ui/widgets/game/main_game_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MainGamePage extends StatelessWidget {
+  const MainGamePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Crypto IDLE')),
+    );
+  }
+}
