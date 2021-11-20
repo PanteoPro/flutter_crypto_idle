e088987d13cba7926b8f1b65638ec373eebec3cf
PanteoPro <panteopro@gmail.com>
2021-11-20 21:44:36 +0300

    small circular main page

--- lib/ui/widgets/game/main_game_page.dart
+++ lib/ui/widgets/game/main_game_page.dart
@@ -194,8 +194,8 @@ class __ActionItemWidget extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           Container(
-            width: 16,
-            height: 16,
+            width: 8,
+            height: 8,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(50),
               border: Border.all(
