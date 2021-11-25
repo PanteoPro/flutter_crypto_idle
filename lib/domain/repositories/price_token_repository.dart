29baa27c5a0901e720cd7345ecfdba3f8e9e01a5
PanteoPro <panteopro@gmail.com>
2021-11-25 16:57:58 +0300

    add token and price token EDR (entity,data,repos)

---
+++ lib/domain/repositories/price_token_repository.dart
@@ -0,0 +1,23 @@
+import 'package:crypto_idle/domain/data_providers/price_token_data_provider.dart';
+import 'package:crypto_idle/domain/entities/price_token.dart';
+
+class PriceTokenRepository {
+  final _priceTokenDataProvider = PriceTokenDataProvider();
+
+  var _prices = <PriceToken>[];
+  List<PriceToken> get prices => List.unmodifiable(_prices);
+  List<PriceToken> pricesByTokenId(int tokenId) => _prices.where((element) => element.tokenId == tokenId).toList();
+
+  Future<void> init() async {
+    await _priceTokenDataProvider.openBox();
+    _updateData();
+  }
+
+  void _updateData() {
+    _prices = _priceTokenDataProvider.loadAllPrices();
+  }
+
+  Future<void> addPrice(PriceToken price) async {
+    await _priceTokenDataProvider.savePrice(price);
+  }
+}
