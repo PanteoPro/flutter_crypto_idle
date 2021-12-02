f8f67b6e689f84bc586820ab208000e68c6bc06c
PanteoPro <panteopro@gmail.com>
2021-12-02 20:32:13 +0300

    add model to main game page

--- lib/ui/navigators/main_navigator.dart
+++ lib/ui/navigators/main_navigator.dart
@@ -11,6 +11,7 @@ import 'package:crypto_idle/ui/widgets/game/view_models/game_market_flat_view_mo
 import 'package:crypto_idle/ui/widgets/game/view_models/game_market_pc_view_model.dart';
 import 'package:crypto_idle/ui/widgets/game/view_models/game_mining_view_model.dart';
 import 'package:crypto_idle/ui/widgets/game/view_models/game_view_model.dart';
+import 'package:crypto_idle/ui/widgets/game/view_models/main_game_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -27,7 +28,10 @@ class MainNavigation {
   String initialRoute() => MainNavigationRouteNames.main;
 
   final routes = <String, Widget Function(BuildContext)>{
-    MainNavigationRouteNames.main: (context) => const MainGamePage(),
+    MainNavigationRouteNames.main: (context) => ChangeNotifierProvider(
+          create: (_) => MainGameViewModel(),
+          child: const MainGamePage(),
+        ),
     MainNavigationRouteNames.gameMarketPC: (context) => ChangeNotifierProvider<GameMarketPCViewModel>(
           create: (_) => GameMarketPCViewModel(),
           child: const GameMarketPCPage(),
