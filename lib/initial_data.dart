8f1d9fb702463e7aefe95081f1d13da2ea47ce3b
PanteoPro <panteopro@gmail.com>
2021-12-04 12:07:54 +0300

    add initialData class

---
+++ lib/initial_data.dart
@@ -0,0 +1,144 @@
+import 'dart:math';
+
+import 'package:crypto_idle/domain/entities/flat.dart';
+import 'package:crypto_idle/domain/entities/pc.dart';
+import 'package:crypto_idle/domain/entities/price_token.dart';
+import 'package:crypto_idle/domain/entities/token.dart';
+
+abstract class InitialDataNames {
+  static const List<String> namesPC = [
+    'LowPriority-D1',
+    'LowPriority-D2',
+    'Старый ПК GH-01',
+    'Старый ПК GH-21',
+    'Roberts Z1',
+    'Roberts Z1-6',
+    'Roberts 2Z-1',
+    'Kertain Brown',
+    'Morty T2000',
+    'Rick K5',
+    'iComp',
+    'iComp 4',
+    'iComp 5s',
+    'iComp 6+',
+    'iComp 13 pro',
+    'MiComp Xm1',
+    'MiComp Portable Zen2',
+    'MiComp FCPU T2',
+    'MiComp i9 Titanic',
+    'Titan',
+    'Titan Complexy',
+    'Titan Full',
+    'Prank PC',
+    'Joker PC',
+    'Griva Edition',
+  ];
+  static const List<String> namesFlat = [
+    'Родительский дом',
+    'Гараж деда',
+    'Подвал',
+    'Дом в лесу',
+    'Просторная квартира',
+    'Футуристичная квартира',
+    'Офис на тверской',
+    'Офис в Москва-Сити',
+    'Коттетж',
+    'Дата центр',
+    'Квартира бабушки',
+  ];
+  static const Map<String, String> namesCrypto = {
+    'ISF': 'Income Soft Frame',
+    'PRS': 'Proof Resistant Secure',
+    'LVZ': 'Level Virus Zombie',
+    'YROON': 'Young Roon',
+    'AWE': 'Awesome weapon edition',
+    'ZZU': 'Zettel Zeus Ultra',
+    'CAT': 'Cat Coin',
+    'SPD': 'Spider Coin',
+    'MZS': 'Moscow Zero Social',
+    'OLEG': 'Outbounded lego',
+    'MCC': 'Minecraft club.clan',
+    'FOAP': 'Fell On A Parrot',
+    'BMF': 'Bit My Fork',
+    'KDGH': 'Kingdom Global Hero',
+    'PDF': 'Piece Drive Fatality ',
+  };
+}
+
+abstract class InitialData {
+  static const int baseCostPC = 100;
+  static const double sellCoefPC = 0.6;
+  static const int basePowerPC = 150;
+  static const int baseEnergyPC = 200;
+
+  static const int baseCostFlat = 1000;
+  static const int baseMonthCostFlat = 200;
+  static const int baseCountPC = 5;
+
+  static const double minTokenPrice = 0.001;
+  static const double maxTokenPrice = 4000.0;
+
+  static List<PC> getInitialPCs() {
+    const names = InitialDataNames.namesPC;
+    var index = 1;
+    final result = names.map((String name) {
+      final cost = (baseCostPC * index).toDouble();
+      final costSell = cost * sellCoefPC;
+      final power = (basePowerPC * index).toDouble();
+      final energy = (baseEnergyPC * index).toDouble();
+      return PC(id: index, name: name, cost: cost, costSell: costSell, power: power, energy: energy);
+    });
+    return result.toList();
+  }
+
+  static List<Flat> getInitialFlats() {
+    const names = InitialDataNames.namesFlat;
+    var index = 1;
+    final result = names.map((String name) {
+      final cost = (baseCostFlat * (index - 1)).toDouble();
+      final costMonth = (baseMonthCostFlat * (index - 1)).toDouble();
+      final countPC = baseCountPC + index - 1;
+      final isBuy = index == 1 || false;
+      final isActive = index == 1 || false;
+      return Flat(
+        id: index,
+        name: name,
+        cost: cost,
+        costMonth: costMonth,
+        countPC: countPC,
+        isBuy: isBuy,
+        isActive: isActive,
+      );
+    });
+    return result.toList();
+  }
+
+  static List<Token> getInitialTokens() {
+    const names = InitialDataNames.namesCrypto;
+    var index = 1;
+    final result = names.entries.map(
+      (e) => Token(
+        id: index,
+        symbol: e.key,
+        fullName: e.value,
+        count: 0,
+        coefMining: 0,
+      ),
+    );
+    return result.toList();
+  }
+
+  static List<PriceToken> getInitialPrices(List<Token> tokens) {
+    final result = tokens.map((Token token) {
+      final cost = double.parse(
+        (minTokenPrice + Random().nextDouble() * (maxTokenPrice - minTokenPrice)).toStringAsFixed(2),
+      );
+      return PriceToken(
+        date: DateTime.now().add(const Duration(days: -1)),
+        cost: cost,
+        tokenId: token.id,
+      );
+    });
+    return result.toList();
+  }
+}
